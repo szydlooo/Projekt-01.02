741b14f282230fb4a7ceef4a1d51e70bdb19b18a
Szydlooo <filis.sadkowski@gmail.com>
2023-02-08 15:25:20 +0100

    new file:   pub/sandbox/img/03a6949de5cbfc95dbe203f10d53e175938cf0929827b31cb27f66ebf104c34826225253289600.webp 	new file:   pub/sandbox/img/03a6949de5cbfc95dbe203f10d53e175938cf0929827b31cb27f66ebf104c34826251698111700.webp 	new file:   pub/sandbox/img/03a6949de5cbfc95dbe203f10d53e175938cf0929827b31cb27f66ebf104c34826251822703400.webp 	deleted:    pub/sandbox/img/bob.jfif 	modified:   pub/sandbox/index.php

--- pub/sandbox/index.php
+++ pub/sandbox/index.php
@@ -11,7 +11,7 @@
         <label for="uploadedFileInput">
             Wybierz plik do wgrania na serwer:
         </label><br>
-        <input type="file" name="uploadedFile" id="uploadedFileInput"><br>
+        <input type="file" name="uploadedFile" id="uploadedFileInput" required><br>
         <input type="submit" value="Wyślij plik" name="submit"><br>
     </form>
 
@@ -21,14 +21,55 @@
     {
         //zdefiniuj folder do którego trafią pliki (ścieżka względem pliku index.php)
         $targetDir = "img/";
+
         //pobierz pierwotną nazwę pliku z tablicy $_FILES
         $sourceFileName = $_FILES['uploadedFile']['name'];
+
         //pobierz tymczasową ścieżkę do pliku na serwerze
         $tempURL = $_FILES['uploadedFile']['tmp_name'];
+
+        //sprawdź czy mamy do czynienia z obrazem
+        $imgInfo = getimagesize($tempURL);
+        if(!is_array($imgInfo)) {
+            die("BŁĄD: Przekazany plik nie jest obrazem!");
+        }
+
+        //wyciągnij pierwotne rozszerzenie pliku
+        //$sourceFileExtension = pathinfo($sourceFileName, PATHINFO_EXTENSION);
+        //zmień litery rozszerzenia na małe
+        //$sourceFileExtension = strtolower($sourceFileExtension);
+        /// niepotrzebne - generujemy webp
+
+        //wygeneruj hash - nową nazwę pliku
+        $newFileName = hash("sha256", $sourceFileName) . hrtime(true)
+                            . ".webp";
+
+        //zaczytujemy cały obraz z folderu tymczasowego do stringa
+        $imageString = file_get_contents($tempURL);
+
+        //generujemy obraz jako obiekt klasy GDImage
+        //@ przed nazwa funkcji powoduje zignorowanie ostrzeżeń
+        $gdImage = @imagecreatefromstring($imageString);
+
+        //wygeneruj pełny docelowy URL
+        $targetURL = $targetDir . $newFileName;
+
         //zbuduj docelowy URL pliku na serwerze
-        $targetURL = $targetDir . $sourceFileName;
+        //$targetURL = $targetDir . $sourceFileName;
+        //wycofane na rzecz hasha
+
+        //sprawdź czy plik przypadkiem już nie istnieje
+        if(file_exists($targetURL)) {
+            die("BŁĄD: Podany plik już istnieje!");
+        }
+
         //przesuń plik do docelowej lokalizacji
-        move_uploaded_file($tempURL, $targetURL);
+        //move_uploaded_file($tempURL, $targetURL);
+        //nieaktualne - generujemy webp
+        imagewebp($gdImage, $targetURL);
+
+
+        echo "Plik został poprawnie wgrany na serwer";
     }
     ?>
 </body>
