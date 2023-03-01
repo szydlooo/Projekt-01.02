f5390ea98904ba3eabade5bdb8f25ef0c9bcad4d
Szydlooo <filis.sadkowski@gmail.com>
2023-03-01 15:09:58 +0100

    modified:   src/Post.class.php

--- src/Post.class.php
+++ src/Post.class.php
@@ -1,14 +1,14 @@
 <?php
 class Post {
     private int $ID;
-    private string $filename;
-    private string $timestamp;
+    private string $FileName;
+    private string $TimeStamp;
     
     function __construct(int $i, string $f, string $t)
     {
         $this->ID = $i;
-        $this->filename = $f;
-        $this->timestamp = $t;
+        $this->FileName = $f;
+        $this->TimeStamp = $t;
     }
 
     static function getLast() : Post {
@@ -26,15 +26,27 @@ class Post {
         $imgInfo = getimagesize($tempFileName);
         if(!is_array($imgInfo)) {
             die("BŁĄD: Przekazany plik nie jest obrazem!");
-        }
+        }   
         $randomNumber = rand(10000, 99999) . hrtime(true);
         $hash = hash("sha256", $randomNumber);
         $newFileName = $targetDir . $hash . ".webp";
         if(file_exists($newFileName)) {
             die("BŁĄD: Podany plik już istnieje!");
-        }
+        }   
         $imageString = file_get_contents($tempFileName);
+        $gdImage = @imagecreatefromstring($imageString);
+        imagewebp($gdImage, $newFileName);
+        global $db;
+        $query = $db->prepare("INSERT INTO post VALUES(NULL, ?, ?)");
+        $dbTimestamp = date("Y-m-d H:i:s");
+        $query->bind_param("ss", $dbTimestamp, $newFileName);
+        if(!$query->execute())
+            die("Błąd zapisu do bazy danych");
+
     }
+}
+
+?>
 }
 
 ?>
