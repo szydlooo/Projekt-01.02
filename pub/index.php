31603ce99e8648b411acd4dd88e240dc474f92b3
Szydlooo <filis.sadkowski@gmail.com>
2023-03-08 15:13:04 +0100

    modified:   composer.json     modified:   composer.lock 	modified:   pub/index.php 	modified:   pub/index.php.old 	modified:   src/config.php 	new file:   src/templates/footer.html.twig 	new file:   src/templates/header.html.twig 	new file:   src/templates/index.html.twig 	new file:   src/templates/upload.html.twig

--- pub/index.php
+++ pub/index.php
@@ -1,11 +1,26 @@
 <?php
-
 require_once('./../src/config.php');
-
 use Steampixel\Route;
-
 Route::add('/', function() {
-    echo "PSZCZOŁA BEZ CZOŁA PSZ ";
+    //strona wyświetlająca obrazki
+    global $twig;
+    $twig->display("index.html.twig");
 });
+Route::add('/upload', function() {
+    //strona z formularzem do wgrywania obrazków
+    global $twig;
+    $twig->display("upload.html.twig");
+});
+
+Route::add('/upload', function() {
+    //wywoła się tylko po otrzymaniu danych metodą post na ten url
+    // (po wypełnieniu formularza)
+    global $twig;
+    if(isset($_POST['submit']))  {
+        Post::upload($_FILES['uploadedFile']['tmp_name']);
+    }
+    $twig->display("index.html.twig");
+}, 'post');
+
 Route::run('/zadanie0102/pub');
 ?>
--- src/config.php
+++ src/config.php
@@ -2,4 +2,8 @@
 require_once('./../vendor/autoload.php');
 $db = new mysqli("localhost", "root", "", "post");
 require("Post.class.php");
+//loader to taki pomocnik do ładowania szablonów
+$loader = new Twig\Loader\FilesystemLoader("./../src/templates");
+//inicjujemy twiga
+$twig = new Twig\Environment($loader);
 ?>
